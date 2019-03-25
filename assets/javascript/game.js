c0e38e2bbffe3993c253f7139b2bf55416c9a266
Patrick Haberern <phaberern@gmail.com>
2019-03-25 14:28:10 -0400

    Main program logic mostly finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,19 @@ var guessesRemaining = 12;
 var currentWord = "";
 var wordPool = []; //I stil have to populate this!
 
-var answers = {
-        MasterChief = "Chief"
-    } // Not sure if I even need this! ^^^^
+// var answers = {
+//         MasterChief = "Chief"
+//     } // Not sure if I even need this! 
 
 
 //Functions!!
 function choooseWord() {
     // RNG is used to index into wordPool and choose a word.
     // Word is then split into an array and each value is displayed at position 4 
+}
+
+function displayLetters() {
+    // displays all instances of letterGuessed in the array at position 4.
 }
 
 function isComplete() {
@@ -36,12 +40,22 @@ function gameReset() {
 // Main program logic!
 
 document.onkeyup = function(event) {
-        var letterGuessed = event.key;
-        if (alreadyGuessed.includes(letterGuessed) {
-                alert("You already guessed that letter!")
-            } else {
-                // display all instances of letter guessed in currentWord;
-                alreadyGuessed.push(letterGuessed);
-            }
-
+    var letterGuessed = event.key;
+    if (alreadyGuessed.includes(letterGuessed)) {
+        alert("You already guessed that letter!");
+    } else {
+        displayLetters();
+        alreadyGuessed.push(letterGuessed);
+        guessesRemaining--;
+        if (guessesRemaining === 0) {
+            gameOver();
+            // resetGame(); ???
         }
+        if (currentWord.isComplete) {
+            gameOver();
+            wins++;
+            // resetGame(); ????
+        }
+    }
+
+}
